f18e944d2ae76c0ab5f3128c71be681f220a9e71
John <johneliud4@gmail.com>
2024-08-22 20:59:19 +0300

    Update: Include code comments where neccessary.

--- calculations/calculations.go
+++ calculations/calculations.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 )
 
+// FindAverage finds the average of a given slice of float64 numbers.
 func FindAverage(nums []float64) float64 {
 	if len(nums) == 0 {
 		return 0
@@ -17,20 +18,25 @@ func FindAverage(nums []float64) float64 {
 	return sum / float64(len(nums))
 }
 
+// FindMedian finds the median of a given slice of float64 numbers.
 func FindMedian(nums []float64) float64 {
 	if len(nums) == 0 {
 		return 0
 	}
 
+	// Sort the slice in ascending order
 	sort.Float64s(nums)
 
 	if len(nums)%2 == 0 {
-		return (nums[len(nums)/2-1] + nums[len(nums)/2]) / 2
+		firstMiddleIndex := len(nums)/2 - 1
+		secondMiddleIndex := firstMiddleIndex + 1
+		return (nums[firstMiddleIndex] + nums[secondMiddleIndex]) / 2
 	} else {
 		return nums[len(nums)/2]
 	}
 }
 
+// FindVariance calculates the variance of a given slice of float64 numbers.
 func FindVariance(nums []float64) float64 {
 	if len(nums) == 0 {
 		return 0
@@ -38,6 +44,7 @@ func FindVariance(nums []float64) float64 {
 
 	mean := FindAverage(nums)
 	variance := 0.0
+
 	for _, num := range nums {
 		result := (num - mean) * (num - mean)
 		variance += result
@@ -45,10 +52,10 @@ func FindVariance(nums []float64) float64 {
 	return variance / float64(len(nums))
 }
 
+// FindStandardDeviation calculates the standard deviation of a given slice of float64 numbers by taking the square root of the variance.
 func FindStandardDeviation(nums []float64) float64 {
 	if len(nums) == 0 {
 		return 0
 	}
-
 	return math.Sqrt(FindVariance(nums))
 }
